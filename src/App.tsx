508d11b5b8a5e09ac7b0027facdf41d0a704f46a
dibrugh <dibrugh@gmail.com>
2023-12-15 16:19:47 +0300

    feat: finish contactus section

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
+import ContactUs from "./scenes/contactUs";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home,
+  );
 
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(false);
 
@@ -15,23 +18,28 @@ function App() {
       // Если элемент на самом верху страницы
       if (window.scrollY === 0) {
         setIsTopOfPage(true);
-        setSelectedPage(SelectedPage.Home)
+        setSelectedPage(SelectedPage.Home);
       }
       if (window.scrollY !== 0) {
         setIsTopOfPage(false);
       }
-    }
+    };
     window.addEventListener("scroll", handleScroll);
     // Размонтирование компонента во избежание багов и утечек памяти
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [])
+  }, []);
 
   return (
     <div className="app bg-gray-20">
-      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
     </div>
   );
 }
